76311c1220e0de9c52757f10697ec544bccdce22
James Hadley <ging19freecss@gmail.com>
2025-09-22 23:59:56 +0200

    cleaned legacy

--- themes/classic_pc/pack/src/main/java/PackBroadcastReceiver.java
+++
@@ -1,6 +0,0 @@
-package com.anysoftkeyboard.themes.classic_pc.pack;
-
-import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
-
-public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
-}
--- themes/three_d/pack/src/main/java/PackBroadcastReceiver.java
+++
@@ -1,6 +0,0 @@
-package com.anysoftkeyboard.themes.three_d.pack;
-
-import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
-
-public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
-}
